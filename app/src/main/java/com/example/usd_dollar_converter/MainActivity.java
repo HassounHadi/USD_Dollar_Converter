e2e711d8d4bcefb09cf6d326f531ed45aae71614
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-02-04 23:41:41 +0200

    didn't make the deadline
    just added some extra things

--- app/src/main/java/com/example/usd_dollar_converter/MainActivity.java
+++ app/src/main/java/com/example/usd_dollar_converter/MainActivity.java
@@ -3,20 +3,22 @@ package com.example.usd_dollar_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-//testing?
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void lira(){
-
+    public void convert(View v){
+        TextView text=findViewById(R.id.link);
+        EditText lebanese=getText(Float.parseFloat());
+        
     }
 
-    public void dollar(){
-
-    }
 }
